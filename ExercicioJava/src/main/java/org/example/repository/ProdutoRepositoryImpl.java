b3b5060c7533b278ad3d85992992c8c41f63778f
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2025-11-04 21:20:02 -0300

    MZ feat: implementa atividade

--- ExercicioJava/src/main/java/org/example/repository/ProdutoRepositoryImpl.java
+++ ExercicioJava/src/main/java/org/example/repository/ProdutoRepositoryImpl.java
@@ -1,32 +1,54 @@
 package org.example.repository;
 
 import org.example.model.Produto;
+import org.example.util.ConexaoBanco;
 
+import java.sql.*;
 import java.util.List;
 
 public class ProdutoRepositoryImpl implements ProdutoRepository{
+
     @Override
-    public Produto save(Produto produto) {
+    public Produto save(Produto produto) throws SQLException {
+        String query = "INSERT INTO teste(nome, preco, quantidade, categoria) VALUES (?,?,?,?)";
+
+        try(Connection conn = ConexaoBanco.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)){
+
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getQuantidade());
+            stmt.setString(4, produto.getCategoria());
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            if(rs.next()){
+                produto.setId(rs.getInt(1));
+            }
+        }catch(SQLException erro){
+            erro.printStackTrace();
+        }
+        return produto;
+    }
+
+    @Override
+    public List<Produto> findAll() throws SQLException {
         return null;
     }
 
     @Override
-    public List<Produto> findAll() {
+    public Produto findById() throws SQLException {
         return null;
     }
 
     @Override
-    public Produto findById() {
+    public Produto update(Produto produto) throws SQLException {
         return null;
     }
 
     @Override
-    public Produto update(Produto produto) {
-        return null;
-    }
-
-    @Override
-    public void deleteById(int id) {
+    public void deleteById(int id) throws SQLException {
 
     }
 }
--- ExercicioJava/src/main/java/org/example/service/ProdutoServiceImpl.java
+++ ExercicioJava/src/main/java/org/example/service/ProdutoServiceImpl.java
@@ -1,13 +1,23 @@
 package org.example.service;
 
 import org.example.model.Produto;
+import org.example.repository.ProdutoRepository;
+import org.example.repository.ProdutoRepositoryImpl;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class ProdutoServiceImpl implements ProdutoService{
+
+    ProdutoRepository repository = new ProdutoRepositoryImpl();
+
     @Override
-    public Produto cadastrarProduto(Produto produto) {
-        return null;
+    public Produto cadastrarProduto(Produto produto) throws SQLException{
+        if(produto.getPreco() < 0){
+            throw new IllegalArgumentException("Preço deve ser positivo.");
+        }
+
+        return repository.save(produto);
     }
 
     @Override
